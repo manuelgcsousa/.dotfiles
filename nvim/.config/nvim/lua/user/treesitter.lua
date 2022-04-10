0ee8af94b606ec5736b75456ef67a3d8e6d55e55
manuelgcsousa <manuel.sousa@tonicapp.com>
2022-04-10 22:15:50 +0100

    nvim -> remove treesitter warning

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -3,11 +3,10 @@
 local treesitter = require("nvim-treesitter.configs")
 
 treesitter.setup{
-    ensure_installed = "maintained",
+    ensure_installed = { "bash", "c", "dockerfile", "html", "java", "json", "lua", "make", "python", "typescript", "vim" },
     sync_install = false,
     ignore_install = { "javascript" },
     highlight = {
-        enable = true,
-        --disable = { "c", "rust" },
+        enable = true
     },
 }
