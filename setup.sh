d8cc6c9cb8e3f26b35013cfb0c2599f9ff0dc32b
manuelgcsousa <manuel.sousa@tonicapp.com>
2022-05-26 14:20:58 +0100

    minor fix

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 # setup .zshenv
 echo ". ~/.config/zsh/.zshenv" > ~/.zshenv
 
-# link with GNU stow.
+# link with GNU stow
 stow -v bspwm dunst kitty nvim rofi zsh
 
-# manually link vscode settings.
+# manually link vscode settings
 [[ $(uname -s) == "Linux" ]] && \
     ln -vf vscode/settings.json $HOME/.config/Code/User/settings.json || \
     ln -vf vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
