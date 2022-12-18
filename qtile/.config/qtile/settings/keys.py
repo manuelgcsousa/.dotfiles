1c54bb63dafed44762996fd07732eb94e9499788
manuelgcsousa <manuel.sousa@tonicapp.com>
2022-12-18 15:08:04 +0000

    [qtile] minor changes to system scripts

--- qtile/.config/qtile/settings/keys.py
+++ qtile/.config/qtile/settings/keys.py
@@ -21,33 +21,34 @@ keys = [
     Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
 
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    # resize windows
+    Key([mod, "control"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
     # window layouts
-    Key([mod], "f", lazy.window.toggle_fullscreen(), desc='Toggle fullscreen'),
-    Key([mod], "t", lazy.window.toggle_floating(), desc='Toggle floating'),
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
+    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating"),
     
     # apps
     Key([mod], "Return", lazy.spawn(f"{HOME}/.cargo/bin/alacritty"), desc="Launch terminal"),
     Key([mod], "space", lazy.spawn("dmenu_run -i -l 10 -fn 'JetBrains Mono Nerd Font:pixelsize=14'"), desc="Move window focus to other window"),
 
-    # system
-    Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
+    # utils
     Key([mod, "shift"], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload qtile's config"),
     Key([mod, "shift"], "e", lazy.spawn(f"{HOME}/.config/qtile/scripts/powermenu"), desc="Exit"),
-    Key([mod, "control", "shift"], "l", lazy.shutdown(), desc="Logout"),
+    Key([mod, "shift"], "p", lazy.spawn(f"{HOME}/.config/qtile/scripts/screenshot"), desc="Printscreen"),
+    Key([mod, "shift"], "b", lazy.spawn(f"{HOME}/.config/qtile/scripts/monitor-bootstrap"), desc="Bootstrap monitor configurations"),
+
+    # system
     Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer set 'Master' 5%+ 1>/dev/null"), desc="Volume Up"),
     Key([], "XF86AudioLowerVolume", lazy.spawn("amixer set 'Master' 5%- 1>/dev/null"), desc="Volume Down"),
     Key([], "XF86AudioMute", lazy.spawn("amixer -D pulse set 'Master' 1+ toggle 1>/dev/null"), desc="Mute"),
     Key([], "XF86MonBrightnessUp", lazy.spawn("brillo -q -A 2"), desc="Laptop brightness up"),
-    Key([], "XF86MonBrightnessDown", lazy.spawn("brillo -q -U 2"), desc="Laptop brightness down"),
-    Key([mod, "shift"], "b", lazy.spawn(f"{HOME}/.config/qtile/scripts/monitor-bootstrap"), desc="Bootstrap monitor configurations")
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brillo -q -U 2"), desc="Laptop brightness down")
 ]
 
 mouse = [
