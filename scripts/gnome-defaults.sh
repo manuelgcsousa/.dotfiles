d4551b463f1ea9f3e368632a9445c7dec26a2ab1
manuelgcsousa <manuel.sousa@tonicapp.com>
2023-08-30 15:32:23 +0100

    [scripts] more gnome defaults

--- scripts/gnome-defaults.sh
+++ scripts/gnome-defaults.sh
@@ -8,8 +8,9 @@ gsettings set org.gnome.shell.keybindings toggle-overview "['<Super>Space']"
 gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>f']"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Shift><Super>q']"
 
-# workspace switching
+# workspace switching and moving
 for i in {1..9}
 do
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Super>$i']"
+    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i "['<Super><Shift>$i']"
 done
